5dbc45d473c5928897210cbca1681ad0f7fe77e9
austinbhan <austinbhan@outlook.com>
2023-10-10 09:13:47 -0700

    Basic fizzbuzz answer

--- src/app.jsx
+++ src/app.jsx
@@ -10,6 +10,24 @@ export default function App() {
   // if divisible by both, fizzbuzz
   // if not divisible by either, return number
 
+  const chosenNumber = prompt('Pick a number!');
+
+  for (let i = 0; i < chosenNumber; i++) {
+    if (i % 3 == 0) {
+      console.log('fizz');
+    }
+    else if (i % 5 == 0) {
+      console.log('buzz');
+    }
+    else if (i % 15 == 0)
+    {
+      console.log('fizzbuzz');
+    }
+    else {
+      console.log(i);
+    }
+  }
+
   return(
     <Router>
       <h1>Hello World</h1>
