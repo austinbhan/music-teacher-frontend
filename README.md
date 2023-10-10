7c942c4b5d4c02ed3822fd732b86a49ee322961e
austinbhan <austinbhan@outlook.com>
2023-10-10 09:06:49 -0700

    Commented fizzbuzz problem

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Step 0, FizzBuzz Test
 - Console.log Results of Fizzbuzz dsna questions
+
+Feature 1: Create millenial table list
+
+Feature 2: Create gen x table list
+
+Feature 3: 
--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,12 @@ import {
 
 export default function App() {
 
+  // Make a basic fizbuzz question
+  // If divisible by 3, fizz
+  // if divisible by 5, buzz
+  // if divisible by both, fizzbuzz
+  // if not divisible by either, return number
+
   return(
     <Router>
       <h1>Hello World</h1>
